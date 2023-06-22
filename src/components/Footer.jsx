c6537881609957b3a2c47a1d258dd66dc68b0e27
shaheerinit <105648279+shaheersystems@users.noreply.github.com>
2023-06-22 21:01:20 +0500

    Footer fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -82,7 +82,7 @@ export default function Footer() {
         </div>
         <div className='mt-8 md:order-1 md:mt-0'>
           <p className='text-center text-xs leading-5 text-gray-500'>
-            &copy; 2020 Your Company, Inc. All rights reserved.
+            &copy; 2020 Worklytics, Inc. All rights reserved.
           </p>
         </div>
       </div>
