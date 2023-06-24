e042f40b66df468e3039006dab1e84c693dbb589
shaheerinit <105648279+shaheersystems@users.noreply.github.com>
2023-06-24 12:26:49 +0500

    auth functionality added

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,177 @@
+import { useEffect, useState } from "react";
+import {
+  Bars3Icon,
+  CalendarIcon,
+  ChartPieIcon,
+  DocumentDuplicateIcon,
+  FolderIcon,
+  HomeIcon,
+  UsersIcon,
+} from "@heroicons/react/24/outline";
+import { useAuth } from "../context/AuthContext";
+const navigation = [
+  { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
+  { name: "Jobs", href: "#", icon: UsersIcon, current: false },
+  { name: "Applications", href: "#", icon: FolderIcon, current: false },
+];
+const teams = [
+  { id: 1, name: "Heroicons", href: "#", initial: "H", current: false },
+  { id: 2, name: "Tailwind Labs", href: "#", initial: "T", current: false },
+  { id: 3, name: "Workcation", href: "#", initial: "W", current: false },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Dashboard() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const { setIsLoggedIn, setUser } = useAuth();
+  const [currentUser, setCurrentUser] = useState(null);
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    if (user) {
+      setCurrentUser(JSON.parse(user));
+    }
+    console.log(currentUser);
+  }, []);
+  const logout = () => {
+    localStorage.removeItem("user");
+    setIsLoggedIn(false);
+    setUser(null);
+  };
+  return (
+    <>
+      {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full bg-gray-50">
+        <body class="h-full">
+        ```
+      */}
+      <div>
+        {/* Static sidebar for desktop */}
+        <div className='hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col'>
+          {/* Sidebar component, swap this element with another sidebar if you like */}
+          <div className='flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6'>
+            <div className='flex h-16 shrink-0 items-center'>
+              <img
+                className='h-8 w-auto'
+                src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
+                alt='Your Company'
+              />
+            </div>
+            <nav className='flex flex-1 flex-col'>
+              <ul role='list' className='flex flex-1 flex-col gap-y-7'>
+                <li>
+                  <ul role='list' className='-mx-2 space-y-1'>
+                    {navigation.map((item) => (
+                      <li key={item.name}>
+                        <a
+                          href={item.href}
+                          className={classNames(
+                            item.current
+                              ? "bg-gray-50 text-indigo-600"
+                              : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+                            "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                          )}
+                        >
+                          <item.icon
+                            className={classNames(
+                              item.current
+                                ? "text-indigo-600"
+                                : "text-gray-400 group-hover:text-indigo-600",
+                              "h-6 w-6 shrink-0"
+                            )}
+                            aria-hidden='true'
+                          />
+                          {item.name}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+                <li>
+                  <div className='text-xs font-semibold leading-6 text-gray-400'>
+                    Settings
+                  </div>
+                  <ul role='list' className='-mx-2 mt-2 space-y-1'>
+                    {teams.map((team) => (
+                      <li key={team.name}>
+                        <a
+                          href={team.href}
+                          className={classNames(
+                            team.current
+                              ? "bg-gray-50 text-indigo-600"
+                              : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+                            "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                          )}
+                        >
+                          <span
+                            className={classNames(
+                              team.current
+                                ? "text-indigo-600 border-indigo-600"
+                                : "text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600",
+                              "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white"
+                            )}
+                          >
+                            {team.initial}
+                          </span>
+                          <span className='truncate'>{team.name}</span>
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+                <li className='-mx-6 mt-auto'>
+                  <a
+                    href='#'
+                    className='flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-50'
+                  >
+                    <img
+                      className='h-8 w-8 rounded-full bg-gray-50'
+                      src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                      alt=''
+                    />
+                    <span className='sr-only'>Your profile</span>
+                    <span aria-hidden='true'>Tom Cook</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+
+        <div className='sticky top-0 z-40 flex items-center gap-x-6 bg-white px-4 py-4 shadow-sm sm:px-6 lg:hidden'>
+          <button
+            type='button'
+            className='-m-2.5 p-2.5 text-gray-700 lg:hidden'
+            onClick={() => setSidebarOpen(true)}
+          >
+            <span className='sr-only'>Open sidebar</span>
+            <Bars3Icon className='h-6 w-6' aria-hidden='true' />
+          </button>
+          <div className='flex-1 text-sm font-semibold leading-6 text-gray-900'>
+            Dashboard
+          </div>
+          <a href='#'>
+            <span className='sr-only'>Your profile</span>
+            <img
+              className='h-8 w-8 rounded-full bg-gray-50'
+              src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+              alt=''
+            />
+          </a>
+        </div>
+
+        <main className='py-10 lg:pl-72'>
+          <div className='px-4 sm:px-6 lg:px-8'>
+            <h1>Hello, welcome {currentUser?.email}</h1>
+            <button onClick={logout}>Logout</button>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+}
