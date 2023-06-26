59b7425cd626401230609a5fd80f5ba1ee1e4247
shaheerinit <105648279+shaheersystems@users.noreply.github.com>
2023-06-26 18:08:13 +0500

    bug fixes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -31,14 +31,6 @@ export default function Dashboard() {
   };
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-100">
-        <body class="h-full">
-        ```
-      */}
       <div className='min-h-full'>
         <Disclosure as='nav' className='bg-white shadow-sm'>
           {({ open }) => (
